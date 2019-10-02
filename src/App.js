0710963b4ff87327ba1ee197e6e0583e32577900
Evandro Assis <evandroassisdev@gmail.com>
2019-10-01 20:52:32 -0400

    feat: Saga

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+
+import './config/ReactotronConfig';
 
 import GlobalStyle from './styles/global';
 import Header from './components/Header';
-
 import Routes from './routes';
+
+import store from './store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <GlobalStyle />
+        <ToastContainer autoClose={3000} />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
